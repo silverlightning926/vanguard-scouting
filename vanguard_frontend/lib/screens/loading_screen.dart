57639de54b13ba5539ee40007b2aeda447beb712
Sidd Rao <siddrao.926@gmail.com>
2023-03-07 09:05:21 -0800

    Removed Boilerplate

---
+++ vanguard_frontend/lib/screens/loading_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
