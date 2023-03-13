4717f252c46f2dee93994e9eb0a36a76b944033a
Sidd Rao <siddrao.926@gmail.com>
2023-03-13 01:34:07 -0700

    Converted Analytics Team Screen To Stateless

--- vanguard_frontend/lib/screens/analytics_team_select_screen.dart
+++ vanguard_frontend/lib/screens/analytics_team_select_screen.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vanguard_frontend/serialized/robot.dart';
 
-class AnalyticsTeamSelectScreen extends StatefulWidget {
+class AnalyticsTeamSelectScreen extends StatelessWidget {
   final List<Robot> robots;
 
   const AnalyticsTeamSelectScreen({super.key, required this.robots});
-
-  @override
-  State<AnalyticsTeamSelectScreen> createState() =>
-      _AnalyticsTeamSelectScreenState();
-}
-
-class _AnalyticsTeamSelectScreenState extends State<AnalyticsTeamSelectScreen> {
-  String searchValue = "";
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +16,14 @@ class _AnalyticsTeamSelectScreenState extends State<AnalyticsTeamSelectScreen> {
           ),
         ),
         body: ListView.builder(
-          itemCount: widget.robots.length,
+          itemCount: robots.length,
           itemBuilder: (context, index) {
             return ListTile(
               shape: Border.all(
                 color: Colors.white,
               ),
-              title: Text(widget.robots[index].name!),
-              subtitle: Text(widget.robots[index].number!),
+              title: Text(robots[index].name!),
+              subtitle: Text(robots[index].number!),
               onTap: () {},
             );
           },
